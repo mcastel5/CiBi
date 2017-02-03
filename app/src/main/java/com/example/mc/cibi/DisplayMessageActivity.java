a305173c054ac351f61190911abe350b987d7275
mcastel5 <mcastel5@binghamton.edu>
2017-02-02 19:02:36 -0500

    Made Buttons Work

--- app/src/main/java/com/example/mc/cibi/DisplayMessageActivity.java
+++ app/src/main/java/com/example/mc/cibi/DisplayMessageActivity.java
@@ -23,9 +23,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
     }
-    public void sendMessage (View view){
-        Intent intent =new Intent(this, eathome.class);
+
+    public void eatbtn(View view) {
+        Intent intent = new Intent(this, eathome.class);
         startActivity(intent);
         // When user clicks 'EAT' button, opens up 'EatHome' screen
+    }
+
+    public void playbtn(View view) {
+        Intent intent = new Intent(this, PlayHome.class);
+        startActivity(intent);
+        // When user clicks 'Play' button, opens up 'PlayHome' screen
+    }
+    public void shopbtn(View view) {
+        Intent intent = new Intent(this, ShopHome.class);
+        startActivity(intent);
+        // When user clicks 'Shop' button, opens up 'ShopHome' screen
+    }
+
+    public void friendsbtn(View view) {
+        Intent intent = new Intent(this, FriendsHome.class);
+        startActivity(intent);
+        // When user clicks 'Friends' button, opens up 'FriendsHome' screen
+    }
+    public void scoresbtn(View view) {
+        Intent intent = new Intent(this, ScoresHome.class);
+        startActivity(intent);
+        // When user clicks 'Scores' button, opens up 'ScoresHome' screen
+    }
+    public void settingsbtn(View view) {
+        Intent intent = new Intent(this, SettingsHome.class);
+        startActivity(intent);
+        // When user clicks 'Settings' button, opens up 'SettingsHome' screen
     }
 }
